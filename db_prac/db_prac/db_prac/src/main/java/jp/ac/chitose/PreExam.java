95550f10476fabc6808c940344b6bd6b1ff14c4e
Maki-ice <b2222110@photon.chitose.ac.jp>
2023-11-20 21:22:13 +0900

    演習3まで終了した

--- db_prac/db_prac/db_prac/src/main/java/jp/ac/chitose/PreExam.java
+++ db_prac/db_prac/db_prac/src/main/java/jp/ac/chitose/PreExam.java
@@ -1,4 +1,20 @@
-package lecture01;
+package jp.ac.chitose;
 
 public class PreExam {
+
+    private String gakusekiCode;
+    private String familyName;
+    private String firstName;
+    private int point;
+
+    public PreExam(String gakusekiCode,String familyName,String firstName,int point){
+        this.gakusekiCode = gakusekiCode;
+        this.familyName = familyName;
+        this.firstName = firstName;
+        this.point = point;
+    }
+
+    public void print(){
+        System.out.println(gakusekiCode+"さんは"+point+"点");
+    }
 }
